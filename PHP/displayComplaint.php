5cc5d8bc59fa8a903ed44578a0e62fdfa983b315
John Hiner <jrwenceslaus@gmail.com>
2017-03-28 10:57:57 -0400

    Minor spelling change + add config.ini for mySQL password:

--- PHP/displayComplaint.php
+++ PHP/displayComplaint.php
@@ -1,6 +1,6 @@
 <?php
 	if($_REQUEST['caseNum'] && $_REQUEST['prefix'])
-	  $dbConn = new mysqli("localHost","root");
+	  $dbConn = new mysqli("localhost","root");
 	  $dbConn->select_db("jcdb");
 	  
 	  $result = $dbConn->query("SELECT * FROM casehistory WHERE caseNumber=".$_REQUEST['caseNum']." LIMIT 1;");
