9b88a847e0abe82aa2e81dbb27a60cce805b4037
John Hiner <Jrwenceslaus@gmail.com>
2017-06-19 17:07:17 -0400

    Intending

--- login.php
+++ login.php
@@ -6,26 +6,26 @@
 ?>
 
 <head>
-<link rel="stylesheet" type="text/css" href="CSS/UI.css">
-<script>
-document.onreadystatechange = function(){
-	if(document.readyState == "complete")
-		document.getElementById("highlightOnLoad").focus();
-}
-</script>
+	<link rel="stylesheet" type="text/css" href="CSS/UI.css">
+	<script>
+		document.onreadystatechange = function(){
+			if(document.readyState == "complete")
+				document.getElementById("highlightOnLoad").focus();
+		}
+	</script>
 </head>
 <div class="centerBox">
-<?php
-	$dbConn = databaseConnect('read');
-	checkRequests($dbConn);
-?>
-<form name='loginButton' method="POST">
-<div>Username</div>
-<div><input id="highlightOnLoad" type="text" name="username" required></input></div>
-<div>Password</div>
-<div><input type="password" name="password" required></input></div>
-<input style='display:none;' type="submit"></input>
-</form>
-<div class="UIButton buttonMedium sideBySide" onclick="document.loginButton.submit();">Log In</div><br>
-<div class="UIButton buttonMedium sideBySide" onclick="location.href='index.php';">Back To Database</div>
+	<?php
+		$dbConn = databaseConnect('read');
+		checkRequests($dbConn);
+	?>
+	<form name='loginButton' method="POST">
+		<div>Username</div>
+		<div><input id="highlightOnLoad" type="text" name="username" required></input></div>
+		<div>Password</div>
+		<div><input type="password" name="password" required></input></div>
+		<input style='display:none;' type="submit"></input>
+	</form>
+	<div class="UIButton buttonMedium sideBySide" onclick="document.loginButton.submit();">Log In</div><br>
+	<div class="UIButton buttonMedium sideBySide" onclick="location.href='index.php';">Back To Database</div>
 </div>
